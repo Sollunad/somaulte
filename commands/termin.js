7b722e753e669fcb7f226dda1bd9f29061ce378d
DanielDaKlaus <daniel.da.klaus@deutschebahn.com>
2018-12-04 16:14:30 +0100

    refactored termin, now reusing embedObject

--- commands/termin.js
+++ commands/termin.js
@@ -1,26 +1,26 @@
 const Discord = require("discord.js");
 
-let embed;
+let embedObject;
 
-exports.run = async (client, message, args) => {
+exports.run = (client, message, args) => {
   const reactionFilter = (reaction, user) => reaction.emoji.name === '✅' || reaction.emoji.name === '❎';
   const emptyString = "Niemand";
 
-   embed = new Discord.RichEmbed({
-        title: 'Nächster Fraktal-Run!',
-        description: `Vorschlag: **${args[0]} Uhr**`,
-        thumbnail: {
-            url: 'https://wiki.guildwars2.com/images/3/38/Daily_Fractals.png'
-        },
-        color: 12470271,
-        fields: [
-            {name: 'Dailies', value: "Lädt..."},
-            {name: 'Zugesagt ✅', value: emptyString},
-            {name: 'Abgesagt ❎', value: emptyString}
-        ]
-    });
+  embedObject = {
+      title: 'Nächster Fraktal-Run!',
+      description: `Vorschlag: **${args[0]} Uhr**`,
+      thumbnail: {
+          url: 'https://wiki.guildwars2.com/images/3/38/Daily_Fractals.png'
+      },
+      color: 12470271,
+      fields: [
+          {name: 'Dailies', value: "Lädt..."},
+          {name: 'Zugesagt ✅', value: emptyString},
+          {name: 'Abgesagt ❎', value: emptyString}
+      ]
+  };
 
-  // add reaction emoji to message
+  const embed = new Discord.RichEmbed(embedObject);
 
   message.channel.send(embed)
   .then(msg => msg.react('✅'))
@@ -31,19 +31,14 @@ exports.run = async (client, message, args) => {
       const dailies = require("../services/dailies.js");
       dailies.fractals.then(fractals => setDailyString(fractals, r.message));
 
-      // createReactionCollector - responds on each react, AND again at the end.
       const collector = r.message
           .createReactionCollector(reactionFilter);
 
-      // set collector events
       collector.on('collect', r => {
-        let embedYesField = Object.assign({}, embed.fields[1]);
-        let embedNoField = Object.assign({}, embed.fields[2]);
-
         if (!r.users.some(user => !user.bot)) return;
 
-        let yes = embedYesField.value.split("\n");
-        let no = embedNoField.value.split("\n");
+        let yes = embedObject.fields[1].value.split("\n");
+        let no = embedObject.fields[2].value.split("\n");
 
         let user = r.users.filter(user => !user.bot).first();
         let reactor = user.username;
@@ -63,55 +58,31 @@ exports.run = async (client, message, args) => {
           }
         }
 
-        embedYesField.value = yes.join('\n');
-        if (embedYesField.value == '') {
-          embedYesField.value = emptyString;
+        embedObject.fields[1].value = yes.join('\n');
+        if (embedObject.fields[1].value == '') {
+            embedObject.fields[1].value = emptyString;
         }
-        embedNoField.value = no.join('\n');
-        if (embedNoField.value == '') {
-          embedNoField.value = emptyString;
+        embedObject.fields[2].value = no.join('\n');
+        if (embedObject.fields[2].value == '') {
+            embedObject.fields[2].value = emptyString;
         }
-
-        // create new embed with old title & description, new field
-        const newEmbed = new Discord.RichEmbed({
-            title: embed.title,
-            description: embed.description,
-            thumbnail: embed.thumbnail,
-            color: embed.color,
-            fields: [
-                embed.fields[0],
-              embedYesField, embedNoField
-            ]
-        });
 
         r.remove(user).catch(console.log);
 
-        // edit message with new embed
-        // NOTE: can only edit messages you author
-        r.message.edit(newEmbed).catch(console.log);
-        embed = newEmbed;
+        const embed = new Discord.RichEmbed(embedObject);
+        r.message.edit(embed).catch(console.log);
       });
   })
-  .catch(console.log);
 };
 
 function setDailyString(fractals, message) {
     const serverEmojis = message.guild.emojis;
     const dailyString = serverEmojis.filter(emoji => fractals.indexOf(emoji.name) != -1).map(emoji => emoji.toString() + " " + emoji.name).join("\n");
 
-    const newEmbed = new Discord.RichEmbed({
-        title: embed.title,
-        description: embed.description,
-        thumbnail: embed.thumbnail,
-        color: embed.color,
-        fields: [
-            {name: 'Dailies', value: dailyString},
-            embed.fields[1], embed.fields[2]
-        ]
-    });
+    embedObject.fields[0].value = dailyString;
 
-    message.edit(newEmbed).catch(console.log);
-    embed = newEmbed;
+    const embed = new Discord.RichEmbed(embedObject);
+    message.edit(embed).catch(console.log);
 }
 
 exports.help = {
